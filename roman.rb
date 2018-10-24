99b60507d9b675e637e27d25bbd55ac8402e8bc2
toomuchespresso <plasticponypoor@gmail.com>
2018-10-23 21:00:28 -0400

    added a loop

--- roman.rb
+++ roman.rb
@@ -1,4 +1,4 @@
- def to_roman(input)
+def to_roman(input)
       roman = { 1 => "I",
       4 => "IV",
       5 => "V",
@@ -12,6 +12,25 @@
       500 => "D",
       900 => "CM",
       1000 => "M" }
-      # returned the value of the key 5
+      # returned the value of the key input
       roman[input]
+
+      solution = "" 
+      while  input >0 do
+            # if input >=10 push x into the solution and reduce input by 10
+            # elseif input >=5 push V into the solution and reduce input by 5
+            if input >= 10
+                  # changes the value of input to be 10 less
+                  input = input - 10
+                  solution << "X" 
+                  
+
+                  #my_str = "Marco "
+                  #my_str << "Polo"
+                  
+            end
+            
+      end
+      # gives you the value of the variable
+      return solution
 end
